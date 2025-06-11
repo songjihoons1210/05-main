52fee327533c7f9b0c288bb0ab20414c83495532
songjihoons1210 <gnsthd0621@naver.com>
2025-06-11 18:17:04 +0900

    ㅁㄴㅇㅁㄴㅇ

---
+++ day11/2_조건문.js
@@ -0,0 +1,76 @@
+/* 문제1
+let 점수1 =prompt("점수입력")*1;
+let 점수2 =prompt("점수입력")*1;
+let 합계 = 점수1 + 점수2 ;
+    if( 합계 >= 90){
+        (console.log("성공");
+}
+        else{
+            console.log("불합격");
+}
+*/
+
+/* 문제 2
+
+let 정수1 = prompt("수 입력 : ")*1;
+let 정수2 = prompt("수 입력 : ")*1;
+    if( 정수1 >= 정수2 ){
+        console.log(`정수1 :${정수1}`)
+    }
+    else{
+        console.log(`정수2 :${정수2}`)
+    }
+
+*/
+
+/* 문제3
+
+let id = prompt("id 입력");
+let pass = prompt("pass 입력");
+    if( id == 'admin' && pass == '1234'){
+        console.log("로그인 성공")
+    }
+    else{
+        console.log("로그인 실패")
+    }
+
+*/
+
+/* 문제4
+let pass = prompt("pass 입력");
+let passw =pass.length;
+if( passw >= 12){console.log("보안 등급 :강함 "); }
+else if( passw >= 8) {console.log("보안 등급: 보통 ");}
+    else{
+        console.log("보안 약함")
+    }
+
+*/
+
+/* 문제5
+
+let seatList = [ 'o', 'x', 'o' ];
+
+
+*/
+
+// 문제6
+l
+let 사용자 = prompt("");
+if(사용자 = 'admin'){ console.log("'모든 기능에 접근할 수 있습니다.'");}
+    else(사용자 = 'editor'){
+        console.log('콘텐츠 수정 및 생성 기능에 접근할 수 있습니다.');
+    }
+    else
+
+editor: '콘텐츠 수정 및 생성 기능에 접근할 수 있습니다.'
+
+viewer: '콘텐츠 조회만 가능합니다.'
+
+그 외 역할: '정의되지 않은 역할입니다.'
+
+
+
+// 문제7
+
+
