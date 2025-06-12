5aa2e36ec212e415a8448d294c862f24e8035621
songjihoons1210 <gnsthd0621@naver.com>
2025-06-12 17:38:55 +0900

    ㅁㄴㅇ

--- day11/2_조건문.js
+++ day11/2_조건문.js
@@ -47,30 +47,57 @@ else if( passw >= 8) {console.log("보안 등급: 보통 ");}
 
 */
 
-/* 문제5
+/*/ 문제5
 
-let seatList = [ 'o', 'x', 'o' ];
+let seatList = ['o', 'x', 'o'];
+let 번호 = prompt("ox 입력 0~1");
+if (seatList[번호] == 'o') {
+    console.log(`예약 성공`);
+} else{
+    console.log("예약 실패");
+    seatList[번호] = 'o'; // 해당하는 좌석목록 상태변경
+}
 
+
+
+
+/*/
+
+/* 문제6
+
+let game = prompt(`게임 점수 :`)*1;
+if(game >= 900) { console.log(`A급 경품`); }
+else if (game >= 700) { console .log('B급'); }
+else if (game >= 500) { console.log(`C급`); }
+else { console.log(`참가상`); }
 
 */
-
-// 문제6
-l
+/* 문제7
 let 사용자 = prompt("");
-if(사용자 = 'admin'){ console.log("'모든 기능에 접근할 수 있습니다.'");}
-    else(사용자 = 'editor'){
-        console.log('콘텐츠 수정 및 생성 기능에 접근할 수 있습니다.');
-    }
-    else
+if (사용자 == 'admin') { console.log("'모든 기능에 접근할 수 있습니다.'"); }
+ else if( 사용자 == 'editor'){ console.log('모든 기능에 접근할 수 있습니다.'); }
+ else if( 사용자 == 'viewer'){ console.log('모 있습니다.'); }
+ else{console.log(`그 외 역할`); }
 
-editor: '콘텐츠 수정 및 생성 기능에 접근할 수 있습니다.'
-
-viewer: '콘텐츠 조회만 가능합니다.'
-
-그 외 역할: '정의되지 않은 역할입니다.'
+ */
 
 
+// 문제 8
+/*
+let 나이 = prompt("나이 입력 :")*1;
+if (나이 >= '65') {console.log(`ㅣ3천ㅣ`);}
+else if(나이 >=20){console.log(`1만`);}
+else if(나이 >=8){console.log(`5천원`);}
+    else{console.log(`무`);}
+*/
+// 문제 9
+/*
+let 성적등급 = prompt(`점수 :`)*1;
+if(성적등급 >= 90) { console.log(`A`); }
+else if (성적등급 >= 80) { console .log('B'); }
+else if (성적등급 >= 70) { console.log(`C`); }
+else { console.log(`재시험`); }
+*/
 
-// 문제7
-
+//문제 10 
 
