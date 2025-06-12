72f53ac74f2037b5f7134c43cfc921c08345400a
songjihoons1210 <gnsthd0621@naver.com>
2025-06-12 17:42:59 +0900

    ㅁㄴㅇ

--- day09/실습/실습문제1.js
+++ day09/실습/실습문제1.js
@@ -2,21 +2,21 @@
 console.log("[주문 접수]")
 
 
-console.log("이름")
+console.log("[이름]")
 let 이름2 = prompt("이름입력")
 console.log(이름2)
 
 
-console.log("메뉴")
+console.log("[메뉴]")
 let 메뉴2 = prompt("메뉴")
 console.log(메뉴2)
 
 
-console.log("수량")
+console.log("[수량]")
 let 수량2 = prompt("수량")
 console.log(수량2)
 
 
-console.log("결제")
+console.log("[결제]")
 let 결제2 = prompt("결제")
 console.log(결제2) 
--- day09/실습/실습문제2.js
+++ day09/실습/실습문제2.js
@@ -2,17 +2,17 @@
 console.log("[운동 기록]");
 
 
-console.log("이름");
+console.log("[이름]");
 let 이름2 = prompt("이름 입력");
 console.log(이름2);
 
 
-console.log("운동종목");
+console.log("[운동종목]");
 let 종류 = prompt("종목적기");
 console.log(종류);
 
 
-console.log("운동시간");
+console.log("[운동시간]");
 let 시간 = prompt("시간(분)");
 console.log(시간);
 
